e5d73ef15cbf287d856b10fefaafb2dea8e1098a
cfprice3 <cfprice3@gmail.com>
2020-03-31 18:56:42 -0400

    fixed app.js.  should ask mgr questions, then prompt for other employees

--- app.js
+++ app.js
@@ -1,74 +1,154 @@
-const Employee = require("./lib/Employee");
+console.log("working");
+
+
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
 const render = require("./lib/htmlRenderer");
-​
 
+const team = [];
 
-inquirer
+function createMgr(){
+  inquirer
   .prompt([
     {
       type: "input",
       message: "What is your name?",
-      name: "Name"
-    },
-    {
-        type: "input",
-        message: "What is your role?",
-        name: "Role"
+      name: "name"
     },
     {
         type: "input",
         message: "What is your ID?",
-        name: "ID"
+        name: "id"
     },
     {
         type: "input",
-        message: "What is your office number (Mrg), GitHub name (Engineer), or School (Intern)?",
-        name: "Role-specific"
+        message: "What is your email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is your office number?",
+        name: "officeNum"
     }
   ])
-  .then(function(response) {
-    
-  });
+  .then(answers =>{
+    // console.log(answers)
+    const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNum)
+    console.log(manager);
+    team.push(manager)
+    createTeam();
+  })
+};
+
+function createTeam (){
+    inquirer.prompt([
+        {
+          type: "list",
+          name: "memberChoice",
+          message: "Which type of team member would you like to add?",
+          choices: [
+            "Engineer",
+            "Intern",
+            "I don't want to add any more team members"
+          ]
+        }
+      ]).then(userChoice => {
+        switch(userChoice.memberChoice) {
+        case "Engineer":
+            createEng();
+        //   addEngineer();
+        console.log("create eng");
+          break;
+        case "Intern":
+            createInt();
+        //   addIntern();
+        console.log("create an intern");
+          break;
+        default:
+        //   buildTeam();
+        console.log("buildteam");
+        }
+      });
+  
+
+};
+
+function createEng(){
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is their name?",
+        name: "name"
+      },
+      {
+          type: "input",
+          message: "What is their ID?",
+          name: "id"
+      },
+      {
+          type: "input",
+          message: "What is their email?",
+          name: "email"
+      },
+      {
+          type: "input",
+          message: "What is their GitHub Account?",
+          name: "github"
+      }
+    ]).then(answers =>{
+        // console.log(answers)
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+        console.log(engineer);
+        team.push(engineer)
+        createTeam();
+        });
+};
+
+    function createInt(){
+        inquirer
+        .prompt([
+          {
+            type: "input",
+            message: "What is their name?",
+            name: "name"
+          },
+          {
+              type: "input",
+              message: "What is their ID?",
+              name: "id"
+          },
+          {
+              type: "input",
+              message: "What is their email?",
+              name: "email"
+          },
+          {
+              type: "input",
+              message: "What is their School?",
+              name: "school"
+          }
+        ]).then(answers =>{
+            // console.log(answers)
+            const intern = new Intern(answers.name, answers.id, answers.email, answers.school)
+            console.log(intern);
+            team.push(intern)
+            createTeam();
+            });
+    };
+
+function buildTeam(){
+
+};
+
+createMgr();
 
 
 
-
-
-
-
-
-
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
